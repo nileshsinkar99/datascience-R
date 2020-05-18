7b4d2f537d15bd13c5af2bfb941efc98a0b373cb
nileshsinkar99 <65572515+nileshsinkar99@users.noreply.github.com>
2020-05-19 05:04:52 +0530

    Add files via upload

---
+++ R as an calculator.R
@@ -0,0 +1,9 @@
+#R as an calculator
+
+1+1 # Simple Arithmetic
+2+3*4 # operator precedence
+3^2 # Exponentiation
+exp(1)# basic mathematical function available
+sqrt(10)
+pi # the constant Pi is pre-defined
+2*pi*6378
