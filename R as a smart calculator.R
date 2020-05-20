0dd60b950ec85420a3748adea25fcdbf936bcdf5
nileshsinkar99 <65572515+nileshsinkar99@users.noreply.github.com>
2020-05-20 06:26:57 +0530

    Add files via upload

---
+++ R as a smart calculator.R
@@ -0,0 +1,10 @@
+x <- 1             #can define variables
+y <- 3             #using " <- " opeartor to set values
+z <- 4
+x*y*z
+
+X*y*z              #variable names are caes sensetive Error:object "X" not found.
+
+This.year <- 2004  #variable names can include period
+This.year
+
