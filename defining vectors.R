8e7223885f5f5b8676a83a62ccd6fe7f2bd7b923
nileshsinkar99 <65572515+nileshsinkar99@users.noreply.github.com>
2020-05-22 09:56:39 +0530

    Add files via upload

---
+++ defining vectors.R
@@ -0,0 +1,9 @@
+rep(1,10)           #repeat the number1,10 times
+seq(2,6)            #sequence of integers between 2 & 6,equivalent to 2:6
+seq(4,20,by=4)      #every 4th integer between 4 & 20
+x <- c(2,0,4,4)     #creates vectors with elements 2,0,0,4
+y <- c(1,9,9,9)     
+x+y                 #sum elements of two vectors
+x*4                 # multiplies elements
+sqrt(x)             #function applies to each element
+                    #Returns vector
